2902be92130eb20b44bc340ed99371cc050a832f
Jviguy <jviguytwo2@gmail.com>
2021-03-08 00:17:08 -0500

    make string.h with basic methods

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,5 @@
 #include "stdio.h"
-#include <string.h>
+#include "string.h"
 
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
@@ -13,7 +13,7 @@ void kernel_main() {
          * his is normal.
          */
 	char msg[100];
-	strcpy(msg, "Hello, kernel World!\n");
-	strcat(msg,  std::AnsiColors().RED);
+	std::strcpy(msg, const_cast<char*>("Hello, kernel World!\n"));
+	std::strcat(msg,  const_cast<char*>(std::AnsiColors().RED));
 	std::print(msg);
 }
