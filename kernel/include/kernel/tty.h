4951a99c2f524f11ed6cc5bda8f43cb61523a4fa
Jviguy <jviguytwo2@gmail.com>
2021-03-12 14:10:46 -0500

    fixes to actual code finally

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -5,17 +5,17 @@
 #include <stdint.h>
 
 #if defined(__cplusplus)
-extern "C" {
-	
+extern "C" 
+{
+#endif
 	void terminal_initialize(void);
 
 	void terminal_putchar(char c);
 	
-	void terminal_putchar(char c);
-
 	void terminal_write(const char* data, size_t size);
 
 	void terminal_writestring(const char* data);
+#if defined(__cplusplus)
 }
 #endif
 #endif
