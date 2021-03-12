bd15ef4e8b9f061a3e5775db0f8532c1236e80ca
Jviguy <jviguytwo2@gmail.com>
2021-03-12 12:15:25 -0500

    pls work

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -5,15 +5,17 @@
 #include <stdint.h>
 
 #if defined(__cplusplus)
-extern "C"
-void terminal_initialize(void);
+extern "C" {
+	
+	void terminal_initialize(void);
 
-void terminal_putchar(char c);
-void terminal_putchar(char c);
+	void terminal_putchar(char c);
+	
+	void terminal_putchar(char c);
 
-void terminal_write(const char* data, size_t size);
+	void terminal_write(const char* data, size_t size);
 
-void terminal_writestring(const char* data);
+	void terminal_writestring(const char* data);
+}
 #endif
-
 #endif
