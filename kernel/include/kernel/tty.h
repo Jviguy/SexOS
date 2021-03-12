406dcf5daaf106de90ccdb55d534c99678ec6084
Jviguy <jviguytwo2@gmail.com>
2021-03-11 22:44:04 -0500

    improving some stuff

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -5,20 +5,15 @@
 #include <stdint.h>
 
 #if defined(__cplusplus)
-extern "C" /* Use C linkage */
-#endif
+extern "C"
 void terminal_initialize(void);
-#if defined(__cplusplus)
-extern "C" /* Use C linkage */
-#endif
+
 void terminal_putchar(char c);
-#if defined(__cplusplus)
-extern "C" /* Use C linkage */
-#endif
+void terminal_putchar(char c);
+
 void terminal_write(const char* data, size_t size);
-#if defined(__cplusplus)
-extern "C" /* Use C linkage */
-#endif
+
 void terminal_writestring(const char* data);
+#endif
 
 #endif
